1d54643791429693e8ca516f902645a58de01bf7
adit.khurana <adit.khurana@hcltech.com>
2025-05-14 10:39:10 +0530

    Logeer Addd

--- src/main/java/com/example/ticket/management/security/JwtAuthFilter.java
+++ src/main/java/com/example/ticket/management/security/JwtAuthFilter.java
@@ -5,6 +5,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,9 +16,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
+@Autowired
+    private static final org.slf4j.Logger logger=  LoggerFactory.getLogger(JwtAuthFilter.class);
+
 
     private final JWTUtil jwtService;
     private final CustomUserDetailsService userDetailsService;
@@ -38,20 +44,20 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             final String username;
 
             if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-                System.out.println("No Authorization header or it doesn't start with Bearer");
+                logger.info("No Authorization header or it doesn't start with Bearer");
                 filterChain.doFilter(request, response);
                 return;
             }
 
             jwt = authHeader.substring(7);
             username = jwtService.extractUserName(jwt);
-            System.out.println("Extracted username: " + username);
+            logger.info("Extracted username: " + username);
 
             if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
                 UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
                 boolean valid = jwtService.validateToken(username,userDetails,jwt);
-                System.out.println("Is token valid: " + valid);
+                logger.info("Is token valid: " + valid);
 
                 if (valid) {
                     UsernamePasswordAuthenticationToken authToken =
@@ -66,7 +72,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                 }
             }
 
-            System.out.println("Auth: " + SecurityContextHolder.getContext().getAuthentication());
+         logger.info("Auth: " + SecurityContextHolder.getContext().getAuthentication());
             filterChain.doFilter(request, response);
         }catch (JwtException | UsernameNotFoundException ex){
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
